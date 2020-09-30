a03bc4c7a3cd76151c6f6192cf8028b389f0d1c5
Mithil Verma <mithilproof@Mithils-MacBook-Pro.local>
2020-10-01 01:26:32 +0530

    Updates

---
+++ precache-manifest.911c921031bff3555c5a3d08f55e33b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9a3f84897933aaa1a9",
+    "url": "/lights-out/static/css/main.783bec0e.chunk.css"
+  },
+  {
+    "revision": "8d9a3f84897933aaa1a9",
+    "url": "/lights-out/static/js/main.8d9a3f84.chunk.js"
+  },
+  {
+    "revision": "498adbd33ac33c4b9d54",
+    "url": "/lights-out/static/js/1.498adbd3.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "9cdca9a15a02775e2d4e3aae44bd963c",
+    "url": "/lights-out/index.html"
+  }
+];
